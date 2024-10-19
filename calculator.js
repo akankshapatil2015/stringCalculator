69f55998c676bd5be52030b4b361836cc0960559
Akanksha Patil <84930402+akankshapatil2015@users.noreply.github.com>
2024-10-19 19:01:09 +0530

    one or two numbers test

--- calculator.js
+++ calculator.js
@@ -1,6 +1,18 @@
 function add(numbers) {
-    if (numbers === "") return 0;
-    return parseInt(numbers);
+  //Check if the input is an empty string
+  if (numbers === "") {
+    return 0;
+  }
+
+  // Split the numbers by comma
+  const numArray = numbers.split(",");
+
+  //Convert the strings to numbers and sum them
+  const sum = numArray.reduce((total, num) => {
+    return total + parseInt(num, 10); // Convert string to integer and sum
+  }, 0);
+
+  return sum;
 }
 
 module.exports = add;
--- calculator.test.js
+++ calculator.test.js
@@ -1,5 +1,13 @@
-const add = require('./calculator');
+const add = require("./calculator");
 
-test('returns 0 for an empty string', () => {
-    expect(add("")).toBe(0);
+test("returns 0 for an empty string", () => {
+  expect(add("")).toBe(0);
+});
+
+test('returns 1 when the string is "1"', () => {
+  expect(add("1")).toBe(1);
+});
+
+test('returns 3 when the string is "1,2"', () => {
+  expect(add("1,2")).toBe(3);
 });
